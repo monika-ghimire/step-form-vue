544ffa3932bf6857ca8046182568a6e9f185619b
monika <suraj@endeavorsoftwaresolutions.com>
2023-03-03 11:46:21 +0545

    as

--- src/main.js
+++ src/main.js
@@ -3,9 +3,17 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import  ElementTiptapPlugin  from 'element-tiptap';
+// import this package's styles
+import 'element-tiptap/lib/index.css';
+
 
 Vue.config.productionTip = false
 Vue.use(ElementUI);
+// use ElementUI's plugin
+Vue.use(ElementTiptapPlugin, {
+  lang: 'zh',
+});
 
 new Vue({
   router,
